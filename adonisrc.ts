3c3a8b551b115c002011078b2ce37ef2929f7486
Lakshay Manglani <lakshaymanglani2212@gmail.com>
2024-07-03 11:41:50 +0530

    chore(linting, formatting): apply code linting and formatting across the codebase

--- adonisrc.ts
+++ adonisrc.ts
@@ -30,7 +30,7 @@ export default defineConfig({
     },
     () => import('@adonisjs/core/providers/vinejs_provider'),
     () => import('@adonisjs/cors/cors_provider'),
-    () => import('@adonisjs/lucid/database_provider')
+    () => import('@adonisjs/lucid/database_provider'),
   ],
 
   /*
